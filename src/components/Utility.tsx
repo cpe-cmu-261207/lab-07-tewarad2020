bd15ca20073793794e20bd5612106f64b9e1371a
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-09-02 16:51:17 +0700

    completed

--- src/components/Utility.tsx
+++ src/components/Utility.tsx
@@ -1,8 +1,10 @@
+import { ClickClear, ClickRandom } from "../stores/PixelPainterStore"
+
 const Utility = () => {
   return (
     <div className="flex justify-center space-x-3">
-      <button className="w-36">Clear</button>
-      <button className="w-36">Random color</button>
+      <button className="w-36" onClick={ClickClear}>Clear</button>
+      <button className="w-36" onClick={ClickRandom}>Random color</button>
     </div>
   )
 }

d228b4cf8914efca48d2c59e00e361d6c31f765b
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-09-02 16:49:44 +0700

    completed

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -4,20 +4,20 @@ const ColorPicker = () => {
   return (
     <div>
       <div className="mx-auto my-4 flex justify-center space-x-4">
-          <SelectColorButton color='#000000' />
-          <SelectColorButton color='#804000' />
-          <SelectColorButton color='#FE0000' />
-          <SelectColorButton color='#FE6A00' />
-          <SelectColorButton color='#FFD800' />
-          <SelectColorButton color='#00FF01' />
+          <SelectColorButton keys={0} color='#000000' />
+          <SelectColorButton keys={1} color='#804000' />
+          <SelectColorButton keys={2} color='#FE0000' />
+          <SelectColorButton keys={3} color='#FE6A00' />
+          <SelectColorButton keys={4} color='#FFD800' />
+          <SelectColorButton keys={5} color='#00FF01' />
         </div>
         <div className="mx-auto my-4 flex justify-center space-x-4">
-          <SelectColorButton color='#FFFFFF' />
-          <SelectColorButton color='#01FFFF' />
-          <SelectColorButton color='#0094FE' />
-          <SelectColorButton color='#0026FF' />
-          <SelectColorButton color='#B100FE' />
-          <SelectColorButton color='#FF006E' />
+          <SelectColorButton keys={6} color='#FFFFFF' />
+          <SelectColorButton keys={7} color='#01FFFF' />
+          <SelectColorButton keys={8} color='#0094FE' />
+          <SelectColorButton keys={9} color='#0026FF' />
+          <SelectColorButton keys={10} color='#B100FE' />
+          <SelectColorButton keys={11} color='#FF006E' />
         </div>
     </div>
   )

566dc95d128505e72d3625ee80e8bfe7549e7dcb
F_ford <76035588+tewarad2020@users.noreply.github.com>
2021-09-02 16:50:53 +0700

    completed

--- src/components/SelectColorButton.tsx
+++ src/components/SelectColorButton.tsx
@@ -1,18 +1,30 @@
+import { PixelPainterStore, toggleselectColorBox } from "../stores/PixelPainterStore"
+
 type SelectColorButtonProps = {
   color: string;
+  keys: number;
 }
 
-const SelectColorButton = ({ color }: SelectColorButtonProps) => {
-
-  //modify this function to highlight correctly
+const SelectColorButton = ({ keys, color }: SelectColorButtonProps) =>  {
+  const state = PixelPainterStore.useState()
+  
   const computeRingSize = () => {
-    return ""
-    return "ring-8 ring-green-400"
+    if (state.statusSelect[keys]) {
+      PixelPainterStore.update(
+        s => {
+          s.color[keys] = color
+        }
+      )
+      return "ring-8 ring-green-400"
+    }
+    else return ''
   }
 
   return (
     <div className={`${computeRingSize()} rounded-md border-black border-2 w-12 h-12 cursor-pointer`}
-      style={{ backgroundColor: color }}
+      style={{ backgroundColor: color }} onClick={() => {
+        toggleselectColorBox(keys)
+      }}
     >
     </div>
   )
